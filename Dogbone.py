c1c3f51e1cca0a35b7e131a6fbc4f1a081b67c68
tony.findeisen <tony.findeisen@onfido.com>
2023-09-19 22:49:01 +0200

    chore: do not set _appPath again

--- Dogbone.py
+++ Dogbone.py
@@ -82,7 +82,6 @@ class DogboneCommand(object):
 
         self.levels = {}
         self.initLogger()
-        _appPath = os.path.dirname(os.path.abspath(__file__))
 
     def writeDefaults(self):
         self.logger.info("config file write")
